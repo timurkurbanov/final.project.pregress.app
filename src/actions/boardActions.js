c626d4ba29867714df035f2f7d3c32c1c95a8bb6
TIMUR <1tkurbanov@gmail.com>
2019-07-12 18:26:43 -0400

    done

---
+++ src/actions/boardActions.js
@@ -0,0 +1,25 @@
+import { CONSTANTS } from "../actions";
+import uuid from "uuidv4";
+
+export const setActiveBoard = id => {
+  return {
+    type: CONSTANTS.SET_ACTIVE_BOARD,
+    payload: id
+  };
+};
+
+export const addBoard = title => {
+  const id = uuid();
+  return {
+    type: CONSTANTS.ADD_BOARD,
+    payload: { title, id }
+  };
+};
+
+export const deleteBoard = title => {
+  const id = uuid();
+  return {
+    type: CONSTANTS.DELETE_BOARD,
+    payload: { title, id }
+  };
+};
